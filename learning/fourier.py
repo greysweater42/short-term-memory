02116783290d6de5113197c4133b0a6cd284b34e
greysweater42 <greysweater42@gmail.com>
2021-10-30 15:43:23 +0200

    learning fourier: better description of ifft

--- learning/fourier.py
+++ learning/fourier.py
@@ -1,30 +1,22 @@
+# %%
 import numpy as np
 from scipy.fft import fft, ifft, fftfreq
 import matplotlib.pyplot as plt
+
+# %% data definition
 half_cycle = np.array([np.pi / 4, np.pi / 2, np.pi * 3 / 4, np.pi])
 x_t = np.concatenate([half_cycle + np.pi * i for i in range(10)])
-
-y = np.round(3 * np.sin(x_t) + np.sin(2 * x_t), 2)
-plt.plot(x_t, y)
+y = np.round(3 * np.sin(x_t) + np.sin(2 * x_t), 2) + 3
 n = len(y)
-d = 10
-
+plt.plot(x_t, y)
+# %% fourier transform
 F = fft(y)
-F_freq = fftfreq(n, 0.125)[: n // 2]  # half_cycle has length of 4, full cycle = 8
-F_freq
-F
-plt.plot(F_freq, 2 / n * np.abs(F[: n // 2]))
-F_freq
-
-# removing noise 2 Hz = sin(2x)
-F = fft(y)
-F
 F_freq = fftfreq(n, 0.125)  # half_cycle has length of 4, full cycle = 8
-F_freq
-plt.plot(F_freq, np.abs(F))
-F[np.abs(F_freq) == 2] = 0
-plt.plot(F_freq, np.abs(F))
+plt.plot(F_freq[: n // 2], 2 / n * np.abs(F[: n // 2]))
+# %% inverse fourier transform
+all(np.isclose(y, ifft(F).real))
 
-ifft(F).real
-plt.plot(x_t, ifft(F).real)
-plt.plot(x_t, y - np.sin(2 * x_t))
+# %% removing noise 2 Hz = sin(2x)
+F[np.abs(F_freq) == 2] = 0
+plt.plot(F_freq[: n // 2], 2 / n * np.abs(F[: n // 2]))
+all(np.isclose(ifft(F).real, y - np.sin(2 * x_t)))
