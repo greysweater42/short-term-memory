83cec4d5a53d54e2b5dcc60a294e51294e9fab06
greysweater42 <greysweater42@gmail.com>
2021-11-09 10:55:18 +0100

    xgb on 3 electrodes

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from src.metrics import calculate_metrics
 import numpy as np
 import mlflow
 from collections import defaultdict
-from tqdm import tqdm
 
 from xgboost import XGBClassifier
 
@@ -12,12 +11,12 @@ def _recursive_defaultdict():
     return defaultdict(_recursive_defaultdict)
 
 
-e = "Fz"
+e = ["Fz", "P3", "P4"]
 bound_high = 1
 bound_low = 40
 smooth = 9
 stride = 4
-phases = ["encoding", "delay"]
+phases = ["delay"]
 letters = [5]
 
 ds = Dataset()
@@ -25,42 +24,31 @@ ds.load_data(
     experiment_types=["R", "M"],
     experiment_times=letters,
     phases=phases,
-    electrodes=[e],
+    electrodes=e,
 )
 # ds.concat_phases()
 ds.create_labels("experiment_type")
-for d in tqdm(ds, desc="fourier transform"):
-    n = 1550 if d.phase == "encoding" else 3300
-    d.fourier_transform(n=n)
+ds.transform_fourier(n=3300)
 ds.process_fourier(bounds=[bound_high, bound_low], smooth=smooth, stride=stride)
-# ds.train_val_divide(val_size=50)
-
-
-obs = _recursive_defaultdict()
-for d in ds:
-    obs[d.person][d.trial][d.phase] = d
+ds.train_val_divide(val_size=50)
 
 x_t = []
-y_t = []
 x_v = []
+y_t = []
 y_v = []
+for ll, d in ds.train:
+    fs = []
+    for el in e:
+        fs.append(d.fouriers[el].processed[0].coef_)
+    x_t.append(np.concatenate(fs))
+    y_t.append(ll)
 
-all_obs = range(1, len(set([d.person for d in ds])) + 1)
-vals = set(np.random.choice(all_obs, 50, replace=False))
-
-for person in obs:
-    for trial in obs[person]:
-        encoding = obs[person][trial]["encoding"].fouriers[e].processed[0].coef_
-        delay = obs[person][trial]["delay"].fouriers[e].processed[0].coef_
-        x = np.concatenate((encoding, delay))
-        y = int(obs[person][trial]["encoding"].experiment_type == "R")
-        if person in vals:
-            x_v.append(x)
-            y_v.append(y)
-        else:
-            x_t.append(x)
-            y_t.append(y)
-
+for ll, d in ds.val:
+    fs = []
+    for el in e:
+        fs.append(d.fouriers[el].processed[0].coef_)
+    x_v.append(np.concatenate(fs))
+    y_v.append(ll)
 
 x_t = np.array(x_t)
 x_v = np.array(x_v)
@@ -73,7 +61,6 @@ def main(x_t, y_t, x_v, y_v):
         eval_metric="logloss",
         max_depth=2,
         subsample=0.7,
-        reg_lambda=10,
         reg_alpha=10,
         colsample_bytree=0.3,
         gamma=10,
@@ -90,6 +77,7 @@ with mlflow.start_run():
         "data": "fourier",
         "phases": phases,
         "letters": [5],
+        "electrodes": e,
     }
     mlflow.set_tags(tags)
     mlflow.log_param("model class", parameters.model_class)
