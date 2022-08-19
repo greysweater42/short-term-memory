34899eb4ca4d39ba7271ecba7c030f0b61b27b6d
greysweater42 <greysweater42@gmail.com>
2022-08-19 20:27:58 +0200

    refactored TrainOrTestDataset into two classes: Train and TestDataset

--- src/dataset/__init__.py
+++ src/dataset/__init__.py
@@ -1,3 +1,3 @@
 from .dataset import Dataset
 from .dataset_config import DatasetConfig
-from .train_or_test_dataset import TrainOrTestDataset
+from .train_and_test_dataset import TrainAndTestDataset, TrainDataset, TestDataset
